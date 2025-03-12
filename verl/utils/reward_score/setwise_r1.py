95942feb267e0d941fd82c8b226d4d8889f230d9
Xueguang Ma <x93ma@edu.uwaterloo.ca>
2025-03-12 00:43:17 -0400

    fix bug

--- verl/utils/reward_score/setwise_r1.py
+++ verl/utils/reward_score/setwise_r1.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 
 def extract_solution(solution_str, method='strict'):
@@ -18,4 +19,8 @@ def compute_score(solution_str, ground_truth, method='strict', format_score=0., 
         score -= 0.5
     elif answer == ground_truth:
         score += 1
+
+    # in random 5% of the cases, print the solution and the ground truth
+    if random.random() < 0.05:
+        print(f"###[Solution]\n{answer}\n\nGround Truth: {ground_truth}\n###")
     return score
